9810a8ba19397297c31c5e0781197689f1b2e738
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-13 15:14:27 -0600

    render loading components if loading is true

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 
 import classnames from "classnames";
+import Loading from "./Loading";
 
 class Dashboard extends Component {
-  state = { loading: true };
+  state = { loading: false };
   render() {
     const dashboardClasses = classnames("dashboard");
 
-    return <main className={dashboardClasses} />;
+    return (
+      <main className={dashboardClasses}>
+        {this.state.loading && <Loading />}
+      </main>
+    );
   }
 }
 
